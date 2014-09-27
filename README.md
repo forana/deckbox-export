3c7dd5045641b1dbd988559402d8e313442e7a2e
forana <alex@alexforan.com>
2014-09-27 15:25:40 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Export script for exporting a single set from [deckbox.org](https://deckbox.org) to a csv file. I'm providing this as-is - this is only intended for my own use, since I'm paranoid about losing my catalog.
+
+## Setup
+
+1. Clone this
+2. `npm install`
+3. Set values in **config.json** as needed.
+4. `node deckbox-export.js`, output will be written to the deckbox-chosen filename inside the export directory you set in config.json.
---
+++ deckbox-export.js
@@ -0,0 +1,65 @@
+var config = require("./config.json"),
+    request = require("request"),
+    q = require("q"),
+    htmlparser = require("htmlparser"),
+    select = require("soupselect").select,
+    fs = require("fs");
+
+var selectSingle = function(html, selector, callback) {
+    var d = q.defer();
+
+    var handler = new htmlparser.DefaultHandler(function (err, dom) {
+        if (err) {
+            d.reject(err);
+        } else {
+            var results = select(dom, selector);
+            d.resolve(results);
+        }
+    });
+
+    var parser = new htmlparser.Parser(handler);
+    parser.parseComplete(html);
+
+    return d.promise;
+};
+
+var authenticityToken;
+
+q.nfcall(request.get, {
+    url: "https://deckbox.org/accounts/login",
+    jar: true
+})
+.spread(function (response, body) {
+    return selectSingle(body, "input[name=authenticity_token]");
+})
+.then(function (results) {
+    authenticityToken = results[0].attribs.value;
+    console.log("authenticity_token = " + authenticityToken);
+    return q.nfcall(request.post, {
+        url: "https://deckbox.org/accounts/login",
+        form: {
+            username: config.username,
+            password: config.password,
+            authenticity_token: authenticityToken
+        },
+        jar: true
+    });
+})
+.spread(function (response, body) {
+    console.log("signed in, exporting set " + config.setID);
+    return q.nfcall(request.get, {
+        url: "https://deckbox.org/sets/export/" + config.setID + "?format=csv&s=&o=&columns=&v2=true",
+        jar: true
+    });
+})
+.spread(function (response, body) {
+    var disposition = response.headers["content-disposition"];
+    console.log(response.headers);
+    var filename = disposition.split('"')[1];
+    console.log("writing " + filename + " (" + body.length + " bytes)");
+    return q.nfcall(fs.writeFile, config.exportsPath + "/" + filename, body);
+})
+.then(function () {
+    console.log("done");
+})
+.catch(console.error);
